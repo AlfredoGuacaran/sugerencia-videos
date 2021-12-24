40c2644388ee9d9bde6dbd92a3fcc0e3bb0510dc
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2021-12-23 22:02:43 -0300

    Codigo clases modulares

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const app = (() => {
   let addVideoPrivate = (url, id) => {
-    $(`#${id}-iframe`).attr('src', url);
+    $(`#${id}`).append(`
+       <iframe id="musica-iframe" width="560" height="315" src="${url}" title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    `);
   };
 
   return {
@@ -68,10 +72,7 @@ const reproductorSerie = new Reproductor(
   'serie'
 );
 
+reproductorMusica.setInicio(60);
 reproductorMusica.playMultimedia();
 reproductorPelicula.playMultimedia();
 reproductorSerie.playMultimedia();
-
-console.log(reproductorMusica.url);
-reproductorMusica.setInicio(60);
-console.log(reproductorMusica.url);
